52784c8532f1f171cf0c1b99245c94dac8c5b285
Bithe <farzana.hossain5@gmail.com>
2023-03-31 15:50:52 +0600

    write done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./pages/home/Home";
 import Topbar from "./components/topbar/Topbar";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
+import Settings from "./pages/settings/Settings";
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
       <Home />
       <Single />
       <Write/>
+
+      <Settings/>
     </div>
   );
 }
---
+++ src/pages/settings/Settings.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Sidebar from '../../components/sidebar/Sidebar'
+import "./Settings.css"
+
+export default function Settings() {
+  return (
+    <div className='settings'>
+
+        <div className="settingsWrapper">
+            jhj
+            <div className="settingsTitle">
+                <span className='settingsUpdateTitle'>Update Your Account</span>
+            </div>
+
+            <Sidebar/>
+        </div>
+      
+    </div>
+  )
+}
